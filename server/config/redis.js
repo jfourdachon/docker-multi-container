8fb96e375389f77887c5c791863418d34bc76b8d
jfourdachon <jfourdachon@yahoo.com>
2021-01-14 22:24:53 +0100

    fix server redis config

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,8 +1,14 @@
 const redis = require('redis');
+const keys = require('../keys')
 
-export const redisClient = redis.createClient({
+const redisClient = redis.createClient({
     host: keys.redisHost,
     port: keys.redisPort,
     retry_stategy: () => 1000,
   });
-  export const redisPublisher = redisClient.duplicate();
+ const redisPublisher = redisClient.duplicate();
+
+ module.exports = {
+     redisClient,
+     redisPublisher
+ }
