eb9e3f486c0b0c824297d4b961f2dc8acbaadd5d
jfourdachon <jfourdachon@yahoo.com>
2021-01-19 22:55:53 +0100

    fix mongoose call

--- api/controllers/values.controller.js
+++ api/controllers/values.controller.js
@@ -1,49 +1,44 @@
-const FibValues = require('../models/fibonacciValues')
-const { redisClient, redisPublisher } = require('../config/redis')
+const FibValues = require('../models/fibonacciValues');
+const { redisClient, redisPublisher } = require('../config/redis');
 const valuesController = {
-    getAllValues: async (_, res) => {
-        try {
-            const response = await FibValues.find()
-            console.log({response})
-            return res.status(200).json({
-                status: 'success',
-                result: reponse.length,
-                data: {
-                  data: response
-                },
-                message: 'Succesfully fetched documents'
-              });
-        } catch (err) {
-            
-        }
-    },
-    getCurrentValue: async(req, res) => {
-        redisClient.hgetall('values', (err, values) => {
-            return res.status(200).json({
-                status: 'success',
-                data: {
-                  data: values
-                },
-              });
-        })
-    },
-    postValue: async (req, res) => {
-        const index = req.body.index
-        if (parseInt(index > 40)) {
-            return res.status(422).json({
-                status:'failed',
-                message: 'Index too high'
-            })
-        }
-        redisClient.hset('values', index, 'Nothing yet')
-        redisPublisher.publish('insert', index)
-        const response = FibValues.values.push(index)
-        await response.save()
-
-        res.json({
-            working: true
-        })
+  getAllValues: async (_, res) => {
+    try {
+      const response = await FibValues.find();
+      return res.status(200).json({
+        data: response,
+      });
+    } catch (err) {
+      console.log({ err });
     }
-}
+  },
+  getCurrentValue: async (req, res) => {
+    redisClient.hgetall('values', (err, values) => {
+      return res.status(200).json({
+        status: 'success',
+        data: values,
+      });
+    });
+  },
+  postValue: async (req, res) => {
+    const index = req.body.index;
+    if (parseInt(index > 40)) {
+      return res.status(422).json({
+        status: 'failed',
+        message: 'Index too high',
+      });
+    }
+    redisClient.hset('values', index, 'Nothing yet');
+    redisPublisher.publish('insert', index);
+    const updatedDoc = await FibValues.findOneAndUpdate({
+      $push: { values: index },
+    });
+    if (!updatedDoc) {
+      await FibValues.create({ values: [index] });
+    }
+    res.json({
+      working: true,
+    });
+  },
+};
 
-module.exports = valuesController
+module.exports = valuesController;
