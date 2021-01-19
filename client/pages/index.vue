4d705c6961181d334a29a240fd7f327223868ed5
jfourdachon <jfourdachon@yahoo.com>
2021-01-19 20:16:49 +0100

    fix development config

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -2,17 +2,13 @@
   <div class="container">
     <b-row>
       <label>Enter your index:</label>
-      <b-form-input
-        v-model="index"
-        type="number"
-        @click="handleSubmit"
-      ></b-form-input>
-      <b-button variant="primary">Submit</b-button>
+      <b-form-input v-model="index" type="number"></b-form-input>
+      <b-button variant="primary" @click="handleSubmit">Submit</b-button>
     </b-row>
     <div>
       <h3>Indices I have seen:</h3>
       {{ indexes }}
-      <h3>Calculated value:</h3>
+      <h3>Calculated values:</h3>
       <div v-for="(value, key) in values" :key="key">
         For index {{ key }}, I calculated {{ value }}
       </div>
@@ -48,12 +44,10 @@ export default {
       const response = await axios.get('/api/values/all')
       this.seenIndexes = response.data
     },
-    methods: {
-      async handleSubmit() {
-        await axios.post('/api/values/value', {
-          index: this.index,
-        })
-      },
+    async handleSubmit() {
+      await axios.post('/api/values/value', {
+        index: this.index,
+      })
     },
   },
 }
