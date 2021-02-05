fa43555e998ce237a686756e9e556e7a7db824e7
jfourdachon <jfourdachon@yahoo.com>
2021-02-05 22:30:11 +0100

    add testing component

---
+++ front/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-container class="main-nav">
+    <b-row align-h="between" align-v="center" class="navbar m-0 p-0">
+      <b-col  class="text-center py-3 border">
+        <nuxt-link to="/">
+          <h5 class="pt-3 pb-3">Home</h5>
+        </nuxt-link>
+      </b-col>
+      <b-col  class="text-center py-3 border">
+        <nuxt-link to="/other-page">
+          <h5 class="pt-3 pb-3">Other page</h5>
+        </nuxt-link>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+
