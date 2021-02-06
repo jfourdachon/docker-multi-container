2a8c60be6ae107880edf896dff8a5e9c521936b1
jfourdachon <jfourdachon@yahoo.com>
2021-02-06 18:49:42 +0100

    fix bootstrap config

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -38,12 +38,10 @@ export default {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    // 'bootstrap-vue/nuxt',
+    'bootstrap-vue/nuxt',
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
   ],
-
-  plugins: ['~/plugins/bootstrap.js'],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
